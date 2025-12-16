750355f5d960b042c8c8a8a3a5718a3c82ee972b
Dmytro Akulov <dmytro.akulov@nixs.com>
2025-12-16 17:45:37 +0100

    started: dance.js

---
+++ 2kyu/dance.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58f58dc3663082a4ba000033/train/javascript
+
+function dance(map) {
+  const init = map.split('\n')
+  console.log(init)
+}
+
+dance('↖→↓←↗\n↑←↓→↓\n↑→S←↓\n↑←↓→↓\n↙→↑←↘')
