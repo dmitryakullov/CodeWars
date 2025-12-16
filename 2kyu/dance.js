30498af6043407cb5488f750fa3c07c6c2865c96
Dmytro Akulov <dmytro.akulov@nixs.com>
2025-12-16 17:51:41 +0100

    init

--- 2kyu/dance.js
+++ 2kyu/dance.js
@@ -1,9 +1,13 @@
 // https://www.codewars.com/kata/58f58dc3663082a4ba000033/train/javascript
 
-function dance(map) {
-  const init = map.split('\n')
-  
-  console.log(init)
+function dance(raw) {
+  const init = raw
+    .split('\n')
+    .map((row, i) =>
+      row.split('').map((cell, j) => ({ row: i, col: j, value: cell, isTouched: false }))
+    );
+
+  console.log(init);
 }
 
-dance('↖→↓←↗\n↑←↓→↓\n↑→S←↓\n↑←↓→↓\n↙→↑←↘')
+dance('↖→↓←↗\n↑←↓→↓\n↑→S←↓\n↑←↓→↓\n↙→↑←↘');
